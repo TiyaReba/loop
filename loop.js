dce1d47479269a6c80da53af399016852a68b503
TiyaReba <105912721+TiyaReba@users.noreply.github.com>
2023-09-15 20:16:59 +0530

    first commit

---
+++ loop.js
@@ -0,0 +1,68 @@
+// print 1 to 5
+// console.log(1);
+// console.log(2)
+// console.log(3)
+// console.log(4)
+// console.log(5);
+
+// for loop
+// for(let i=0;i<5;i++){
+//     console.log(i+1)
+// }
+
+// printing array elements
+
+// for(let i = 0; i<fruits.length;i++){
+//     console.log(fruits[i])
+// }
+
+// for in and for of
+// for(let i in fruits){
+//     console.log(fruits[i]);
+// }
+
+let person ={
+    name:"Tiya",
+    age:10
+}
+
+// for(let i in person){
+//     console.log(person[i])
+// }
+
+// for(let i of fruits){
+//     console.log(i)
+// }
+// var myName = "Tiya";
+// for(let i of myName){
+//     console.log(i)
+// }
+
+// While loop
+// print 1 to 5
+// let i = 1;
+// while(i<=5){
+//     console.log(i)
+//     i++
+// }
+// let fruits = ["Apple","Mango","Orange","Grapes"]
+// let i = 0;
+// while(i<fruits.length){
+//     console.log(fruits[i]);
+//     i++
+// }
+
+// do while
+// print 1-5
+// let i =1
+// do{
+//     console.log(i);
+//     i++
+// }while(i<=5)
+
+let fruits = ["Apple","Mango","Orange","Grapes"]
+let i = 0;
+do {
+    console.log(fruits[i])
+    i++
+} while (i<fruits.length);
